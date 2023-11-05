400634a45e8eb7c4645c21acdae4329e80e4f496
Jeremy <knightj0044@gmail.com>
2023-11-05 14:52:25 -0600

    initializing project

---
+++ src/main/java/com/jknigh4/javacalculatorapp/JavaCalculatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.jknigh4.javacalculatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCalculatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCalculatorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jknigh4/javacalculatorapp/JavaCalculatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jknigh4.javacalculatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCalculatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
